79c4f0ad1f5a2d4af7da3dc23feaab4726da8ac5
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2023-09-30 20:21:24 -0300

    Agregando archivos sin seguimiento

---
+++ A-NotebookUI/A_NotebookUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_NotebookUIApp.swift
+//  A-NotebookUI
+//
+//  Created by Sebastian Marquez Rojas on 30/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct A_NotebookUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A-NotebookUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A-NotebookUI
+//
+//  Created by Sebastian Marquez Rojas on 30/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
