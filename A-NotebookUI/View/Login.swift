5e2e896c7a8a7657bf1e2e300b457e81ec1f08b9
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2023-10-03 17:40:31 -0300

    visualizacion del login

---
+++ A-NotebookUI/View/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  A-NotebookUI
+//
+//  Created by Sebastian Marquez Rojas on 03/10/2023.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
