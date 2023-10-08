1f9bb22a60a44e9957d79be20acfc6c2e584f70f
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2023-10-07 21:10:15 -0300

    agregados usuarios consumiendo api para traerlos

---
+++ A-NotebookUI/Model/NewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewModel.swift
+//  A-NotebookUI
+//
+//  Created by Sebastian Marquez Rojas on 06/10/2023.
+//
+
+import Foundation
---
+++ A-NotebookUI/ViewModel/NewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewViewModel.swift
+//  A-NotebookUI
+//
+//  Created by Sebastian Marquez Rojas on 06/10/2023.
+//
+
+import Foundation
